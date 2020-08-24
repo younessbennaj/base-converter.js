30a835238d50e29370e0b3645b68e7b1b7fe3213
Youness BENNAJ <youness.bennaj@gmail.com>
2020-08-24 18:02:28 +0200

    write our first module and generate the bundle

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-function sum(a, b) {
-    return a + b;
-}
+import sum from './sum';
+
+console.log(sum(3, 4));
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "src/index.js",
+    entry: "./src/index.js",
     // must be an absolute path (use the Node.js path module)
     output: {
         path: path.resolve(__dirname, "dist"), // string
-    },
-    filename: "bundle.js",
+        filename: "bundle.js"
+    }
 };
