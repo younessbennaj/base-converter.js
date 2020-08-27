4be45f172632f18e72bafa5ef7fa99c990caf125
Youness BENNAJ <youness.bennaj@gmail.com>
2020-08-27 15:52:13 +0200

    add a test case when we submit a conversion to retrieve a result inside the textarea

--- src/__tests__/ConvertForm.test.js
+++ src/__tests__/ConvertForm.test.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { render, screen, getByRole } from '@testing-library/react';
+import { render, screen, getByRole, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import App from '../components/App';
 
-describe('Converter form', () => {
-    // test suite block
+// utils
+import { convertFromTo10 } from '../utils/base10Converter';
+
+let form, group, numberInput, submit;
+
+beforeEach(() => {
 
     render(<App />);
 
-    const form = screen.getByRole('form');
+    form = screen.getByRole('form');
 
     //option radio button group 
-    const group = screen.getByRole('group');
+    group = screen.getByRole('group');
 
-    const numberInput = screen.getByLabelText('Number:');
+    numberInput = screen.queryByLabelText('Number:');
 
-    const submit = screen.getByRole('button', { name: /submit/i });
+    submit = screen.getByRole('button', { name: /submit/i });
+
+    result = screen.getByRole('textbox', { name: /result:/i });
+
+})
+
+describe('Converter form', () => {
+    // test suite block
 
     test('Converter Form is displayed in the DOM', () => {
 
@@ -30,5 +41,19 @@ describe('Converter form', () => {
         expect(form).toContainElement(numberInput);
         expect(form).toContainElement(submit);
 
+    })
+
+    test('Converter form should return a result inside the textarea when it is submit', () => {
+
+        //User type a number
+        userEvent.type(numberInput,
+            '100010101'
+        );
+
+        //User click on the submit button 
+        userEvent.click(submit);
+
+        //We should return a value inside the result textarea 
+        expect(result.value).toBe(convertFromTo10(numberInput.value, 2));
     })
 })
