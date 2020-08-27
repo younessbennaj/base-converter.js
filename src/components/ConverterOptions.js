89d1e4c8a071430e15a396462e726967129f5cd3
Youness BENNAJ <youness.bennaj@gmail.com>
2020-08-27 20:00:43 +0200

    add a new converting option to convert from 10 to 16

--- src/components/ConverterOptions.js
+++ src/components/ConverterOptions.js
@@ -3,10 +3,12 @@ import React from 'react';
 const ConverterOptions = ({ handleOptionChange }) => {
     return (
         <fieldset onChange={handleOptionChange}>
-            <input value={2} defaultChecked={true} type="radio" name="base" id="base2" />
+            <input value={JSON.stringify({ from: 2, to: 10 })} defaultChecked={true} type="radio" name="base" id="base2" />
             <label className="label" htmlFor="base2">From base 2 to base 10</label><br />
-            <input value={10} type="radio" name="base" id="base10" />
+            <input value={JSON.stringify({ from: 10, to: 2 })} type="radio" name="base" id="base10" />
             <label className="label" htmlFor="base10">From base 10 to base 2</label><br />
+            <input value={JSON.stringify({ from: 10, to: 16 })} type="radio" name="base" id="base16" />
+            <label className="label" htmlFor="base10">From base 10 to base 16</label><br />
         </fieldset>
     );
 }
